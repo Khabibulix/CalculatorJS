141366bc8993b25ded90156d444adc6c6e16e3a6
Khab <unitetempete@gmail.com>
2024-05-01 05:29:26 +0200

    Adding event listeners to all numbers to display the value on the calculator screen

--- script.js
+++ script.js
@@ -1,35 +1,8 @@
-// function add(numberOne, numberTwo) {
-//     if (Number.isInteger(numberOne) && Number.isInteger(numberTwo)){
-//         return numberOne + numberTwo;
-//     }
-// }
-
-// function subtract(numberOne, numberTwo) {
-//     if (Number.isInteger(numberOne) && Number.isInteger(numberTwo)){
-//         return numberOne - numberTwo;
-//     }
-// }
-
-// function multiply(numberOne, numberTwo) {
-//     if (Number.isInteger(numberOne) && Number.isInteger(numberTwo)){
-//         return numberOne * numberTwo;
-//     }
-// }
-
-// function divide(numberOne, numberTwo) {
-//     if (Number.isInteger(numberOne) && Number.isInteger(numberTwo)){
-//         return numberOne / numberTwo;
-//     }
-// }
-
-// console.log(add(3, 4)); // 7
-// console.log(subtract(6, 4)); // 2
-// console.log(multiply(3, 4)); // 12
-// console.log(divide(16, 4)); //4
-
 let operator = "*";
 let numberOne = 5;
 let numberTwo = 6;
+const NUMBERS = document.querySelectorAll(".number");
+let screen = document.querySelector(".screen");
 
 function operate(operation, numberOne, numberTwo) {
     if (Number.isInteger(numberOne + numberTwo)) {
@@ -46,4 +19,8 @@ function operate(operation, numberOne, numberTwo) {
     }    
 }
 
-console.log(operate(operator, numberOne, numberTwo));
+NUMBERS.forEach(function(item) {
+    item.addEventListener("click", () =>{
+        screen.textContent = item.id;
+    });
+});
