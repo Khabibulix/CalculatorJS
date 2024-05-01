0bb9e38397f7200231525a72ff4094f08ef84fc4
Khab <unitetempete@gmail.com>
2024-05-01 22:22:28 +0200

    Behaviour is correct for most of operations, str numbers are correctly converted, now moving on to handling divisions

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
 const NUMBERS = document.querySelectorAll(".number");
 const OPERATIONS = document.querySelectorAll(".operations-container div")
+
 let clearButton = document.querySelector("#clear");
 let screen = document.querySelector(".screen");
+let equalButton = document.querySelector("#equal");
 let numberHistory = [];
 let currentOperation = "";
 
 function operate(operation, numberOne, numberTwo) {
-    if (Number.isInteger(numberOne + numberTwo)) {
-        switch (operation){
-            case '+':
-                return numberOne + numberTwo;
-            case '-':
-                return numberOne - numberTwo;
-            case '*':
-                return numberOne * numberTwo;
-            case '/':
-                return numberOne / numberTwo;
-        }
-    }    
+    numberOne = parseInt(numberOne);
+    numberTwo = parseInt(numberTwo);
+    switch (operation){
+        case '+':
+            return numberOne + numberTwo;
+        case '-':
+            return numberOne - numberTwo;
+        case '*':
+            return numberOne * numberTwo;
+        case '/':
+            // code ici pour divisions cheloues
+            return numberOne / numberTwo;
+    }
 }
 
 NUMBERS.forEach(function(item) {
     item.addEventListener("click", () =>{
-        screen.textContent = item.id;
         numberHistory.push(item.id);
     });
 });
@@ -35,4 +37,11 @@ OPERATIONS.forEach(function(item) {
 
 clearButton.addEventListener("click", () => {
     screen.textContent = "";
+});
+
+equalButton.addEventListener("click", () => {
+    let result = operate(currentOperation, numberHistory[0], numberHistory[1]);
+    numberHistory.length = 0;
+    screen.textContent = result;
+    numberHistory.push(result);
 });
