05e63709b5d74f3cca3627df29bc77e009b82163
Khab <unitetempete@gmail.com>
2024-05-01 04:34:14 +0200

    Creating bases functions for all operations

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-alert("Test");
+function add(numberOne, numberTwo) {
+    if (Number.isInteger(numberOne) && Number.isInteger(numberTwo)){
+        return numberOne + numberTwo;
+    }
+}
+
+function subtract(numberOne, numberTwo) {
+    if (Number.isInteger(numberOne) && Number.isInteger(numberTwo)){
+        return numberOne - numberTwo;
+    }
+}
+
+function multiply(numberOne, numberTwo) {
+    if (Number.isInteger(numberOne) && Number.isInteger(numberTwo)){
+        return numberOne * numberTwo;
+    }
+}
+
+function divide(numberOne, numberTwo) {
+    if (Number.isInteger(numberOne) && Number.isInteger(numberTwo)){
+        return numberOne / numberTwo;
+    }
+}
+
+console.log(add(3, 4)); // 7
+console.log(subtract(6, 4)); // 2
+console.log(multiply(3, 4)); // 12
+console.log(divide(16, 4)); //4
